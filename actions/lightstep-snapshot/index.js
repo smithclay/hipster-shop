efdb83dcbfb9c166f1f1103264016cea7b638b0c
Clay Smith <smithclay@gmail.com>
2020-08-20 10:26:09 -0700

    add pagerduty on-call status

--- actions/lightstep-snapshot/index.js
+++ actions/lightstep-snapshot/index.js
@@ -1,15 +1,42 @@
 const core = require('@actions/core');
-
 const lightstepSdk = require('lightstep-js-sdk');
 
+/**
+ * Resolves input as an enviornment variable or action input
+ * @param {*} name input name
+ */
+const resolveActionInput = (name, config = {}) => {
+  if (typeof name !== 'string') {
+      return null
+  }
+  const configName = name.replace('lightstep_', '')
+  return process.env[name.toUpperCase()] || core.getInput(name) || config[configName]
+}
+
+/**
+* Fails action if input does not exist
+* @param {*} name input name
+*/
+const assertActionInput = (name, config) => {
+  if (!resolveActionInput(name, config)) {
+      core.setFailed(
+          `Input ${name} must be set as an env var, passed as an action input, or specified in .lightstep.yml`)
+  }
+}
 
 async function run() {
     try {
-      const lightstepOrg = process.env.LIGHTSTEP_ORG;
-      const lightstepProj = process.env.LIGHTSTEP_PROJECT || process.env.LIGHTSTEP_PROJ;
-      const lightstepService = process.env.LIGHTSTEP_SERVICE;
+      assertActionInput('lightstep_api_key')
+      assertActionInput('lightstep_organization')
+      assertActionInput('lightstep_project')
+      assertActionInput('lightstep_service')
 
-      const apiClient = await lightstepSdk.init(lightstepOrg, process.env.LIGHTSTEP_API_TOKEN);
+      const apiKey = resolveActionInput('lightstep_api_key')
+      const lightstepOrg = resolveActionInput('lightstep_organization')
+      const lightstepProj = resolveActionInput('lightstep_project')
+      const lightstepService = resolveActionInput('lightstep_service')
+
+      const apiClient = await lightstepSdk.init(lightstepOrg, apiKey);
       console.log(`Creating snapshot for project ${lightstepProj}...`);
       const newSnapshot = await apiClient.sdk.apis.Snapshots.createSnapshot({ 
         organization: lightstepOrg, 
@@ -25,13 +52,8 @@ async function run() {
 
       core.setOutput('lightstep_snapshot_id', newSnapshot.body.data.id);
     } catch (error) {
-        core.setFailed(error.message);
+      core.setFailed(error.message);
     }
-}
-
-if (!process.env.LIGHTSTEP_API_TOKEN) {
-    core.setFailed('env LIGHTSTEP_API_TOKEN must be set');
-    return;
 }
 
 run();
